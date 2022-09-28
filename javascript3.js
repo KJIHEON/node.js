d6d98a7cb90015274c91570b0a7543bed20b382d
김지헌 <youwa65@gmail.com>
2022-09-28 19:06:25 +0900

    map을 이용한 배열 문제

--- javascript3.js
+++ javascript3.js
@@ -552,10 +552,15 @@
 
 //자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
 //예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다. 
-let n = 123;
-n = String(n)
-answer = 0;
-for(i = 0; i< n.length; i++){
-    answer += Number(n[i])
-}
-console.log(String(answer))
+// let n = 123;
+// n = String(n)
+// answer = 0;
+// for(i = 0; i< n.length; i++){
+//     answer += Number(n[i])
+// }
+// console.log(String(answer))
+
+let n = 12345;
+console.log([...n+""].map(a=>+a).reverse())
+console.log([...n+""])
+console.log([n+""].map(a=>-a))
